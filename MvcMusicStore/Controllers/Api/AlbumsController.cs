cfb0fcbc5c091799e0bc693c0cd9aae5db11182e
Jeff V <rally25rs@yahoo.com>
2012-10-11 14:07:02 -0400

    Added KendoUI files. Added WebAPI.

---
+++ MvcMusicStore/Controllers/Api/AlbumsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers.Api
+{
+    public class AlbumsController : ApiController
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        // GET api/albums
+        public IEnumerable<Album> Get()
+        {
+            storeDB.Configuration.ProxyCreationEnabled = false;
+            return storeDB.Albums;
+        }
+
+        // GET api/albums/5
+        public Album Get(int id)
+        {
+            storeDB.Configuration.ProxyCreationEnabled = false;
+            return storeDB.Albums.Single(x => x.AlbumId == id);
+        }
+
+        // POST api/albums
+        public void Post([FromBody]Album value)
+        {
+            storeDB.Albums.Add(value);
+            storeDB.SaveChanges();
+        }
+
+        // PUT api/albums/5
+        public void Put(int id, [FromBody]Album value)
+        {
+            var albumToUpdate = storeDB.Albums.Single(x => x.AlbumId == id);
+            albumToUpdate.AlbumArtUrl = value.AlbumArtUrl;
+            albumToUpdate.ArtistId = value.ArtistId;
+            albumToUpdate.GenreId = value.GenreId;
+            albumToUpdate.Price = value.Price;
+            albumToUpdate.Title = value.Title;
+            storeDB.SaveChanges();
+        }
+
+        // DELETE api/albums/5
+        public void Delete(int id)
+        {
+            var albumToRemove = storeDB.Albums.Single(x => x.AlbumId == id);
+            storeDB.Albums.Remove(albumToRemove);
+            storeDB.SaveChanges();
+        }
+    }
+}
---
+++ MvcMusicStore/Controllers/Api/GenresController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers.Api
+{
+    public class GenresController : ApiController
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        // GET api/genres
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse<IEnumerable<Genre>>(HttpStatusCode.OK, storeDB.Genres);
+        }
+
+        // GET api/genres/5
+        public HttpResponseMessage Get(int id)
+        {
+            return Request.CreateResponse<Genre>(HttpStatusCode.OK, storeDB.Genres.Single(x => x.GenreId == id));
+        }
+
+        // POST api/genres
+        public void Post([FromBody]Genre value)
+        {
+            storeDB.Genres.Add(value);
+            storeDB.SaveChanges();
+        }
+
+        // PUT api/genres/5
+        public void Put(int id, [FromBody]Genre value)
+        {
+            var toUpdate = storeDB.Genres.Single(x => x.GenreId == id);
+            toUpdate.Name = value.Name;
+            toUpdate.Description = value.Description;
+            storeDB.SaveChanges();
+        }
+
+        // DELETE api/genres/5
+        public void Delete(int id)
+        {
+            var toDelete = storeDB.Genres.Single(x => x.GenreId == id);
+            storeDB.Genres.Remove(toDelete);
+            storeDB.SaveChanges();
+        }
+    }
+}
