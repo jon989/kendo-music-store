e2c670b56a2e7ae949e246410121b8d77634b0f3
Jeff V <rally25rs@yahoo.com>
2012-11-29 15:31:01 -0500

    Added cart checkout API.

---
+++ MvcMusicStore/Controllers/Api/CheckoutApiController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers.Api
+{
+    public class CheckoutApiController : ApiController
+    {
+        readonly MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        public object Post(ApiOrderSubmit orderSubmit)
+        {
+            try
+            {
+                var order = orderSubmit.Order;
+                order.Username = User.Identity.Name;
+                order.OrderDate = DateTime.Now;
+
+                //Add the Order
+                storeDB.Orders.Add(order);
+
+                //Process the order
+                var cart = ShoppingCart.GetCart(storeDB, HttpContext.Current);
+                var cartItems = BuildCartItems(orderSubmit.Items);
+                cart.CreateOrder(order, cartItems);
+
+                // Save all changes
+                storeDB.SaveChanges();
+
+                return new
+                {
+                    error = ""
+                };
+            }
+            catch
+            {
+                return new
+                {
+                    error = "There was an error submitting your order."
+                };
+            }
+        }
+
+        private List<Cart> BuildCartItems(ApiOrderItem[] orderItems)
+        {
+            return orderItems.Select(x => new Cart { AlbumId = x.albumId, Count = x.quantity }).ToList();
+        }
+    }
+}
---
+++ MvcMusicStore/Models/ApiOrderSubmit.cs
@@ -0,0 +1,15 @@
+﻿
+namespace MvcMusicStore.Models
+{
+    public class ApiOrderItem
+    {
+        public int albumId { get; set; }
+        public int quantity { get; set; }
+    }
+
+    public class ApiOrderSubmit
+    {
+        public Order Order { get; set; }
+        public ApiOrderItem[] Items { get; set; }
+    }
+}
