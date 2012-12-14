c24e98ec88cbd8eadcabdf8890cdf6b7dda311ec
Jeff V <rally25rs@yahoo.com>
2012-12-14 10:45:00 -0500

    mobile page styling improvements.

--- MvcMusicStore/Scripts/App/home-mobile.js
+++ MvcMusicStore/Scripts/App/home-mobile.js
@@ -1,11 +1,8 @@
 ﻿(function ($, kendo) {
     $("#mobile-device-selection").kendoDropDownList({
         change: function (e) {
-            var value = this.value();
-            //var value = $("#mobile-device-selection").val();
-            $(".get-mobile-panel").fadeOut(400, function () {
-                $("#get-mobile-" + value).fadeIn();
-            });
+            $(".get-mobile-panel").hide();
+            $("#get-mobile-" + this.value()).fadeIn();
         }
     });
 })(jQuery, kendo);
