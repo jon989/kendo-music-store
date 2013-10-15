7f3f9695585717eef57d96fc80381337c9e0be89
Jimmy Byrd <jimmybyrd@gmail.com>
2013-10-15 14:15:37 -0400

    Added baseUrl to api calls

--- MvcMusicStore/Scripts/App/config.js
+++ MvcMusicStore/Scripts/App/config.js
@@ -1,20 +1,20 @@
 ﻿function Config() {
     // URLs to WebAPI Controllers
-    this.albumsUrl = "/Services/MusicStore.svc/Albums";
-    this.albumsWithArtistsUrl = "/Services/MusicStore.svc/Albums?$expand=Artist";
-    this.artistsUrl = "/Services/MusicStore.svc/Artists";
-    this.genresUrl = "/Services/MusicStore.svc/Genres";
-    this.imagesUrl = "/Api/Images";
-    this.salesByGenreUrl = "/Api/StoreSalesGenre";
-    this.salesAndRevenueUrl = "/Api/StoreSalesRevenue";
+    this.albumsUrl = baseUrl + "/Services/MusicStore.svc/Albums";
+    this.albumsWithArtistsUrl = baseUrl + "/Services/MusicStore.svc/Albums?$expand=Artist";
+    this.artistsUrl = baseUrl + "/Services/MusicStore.svc/Artists";
+    this.genresUrl = baseUrl + "/Services/MusicStore.svc/Genres";
+    this.imagesUrl = baseUrl + "/Api/Images";
+    this.salesByGenreUrl = baseUrl + "/Api/StoreSalesGenre";
+    this.salesAndRevenueUrl = baseUrl + "/Api/StoreSalesRevenue";
     
     // General Settings
     this.albumDetailsWindowWidth = "400px";
     this.featuredArtist = "Metallica";
     this.bannerImages = [
-        "/Content/Images/Feature1.png",
-        "/Content/Images/Feature2.png",
-        "/Content/Images/Feature3.png"
+        baseUrl + "/Content/Images/Feature1.png",
+        baseUrl + "/Content/Images/Feature2.png",
+       baseUrl + "/Content/Images/Feature3.png"
     ];
     this.browseGenrePageSize = 20;
     this.manageAlbumsGridPageSize = 50;
